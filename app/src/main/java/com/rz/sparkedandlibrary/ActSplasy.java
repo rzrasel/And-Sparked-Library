bb187940151cec617b8eb645d920220744294813
Rz Rasel <Rz Rasel>
2017-08-24 02:31:10 +0600

    Test

---
+++ app/src/main/java/com/rz/sparkedandlibrary/ActSplasy.java
@@ -0,0 +1,13 @@
+package com.rz.sparkedandlibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActSplasy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_splasy);
+    }
+}
