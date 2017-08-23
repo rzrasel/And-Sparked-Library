baa0d5eb0fbfda0bd49c0a8fe742d25414b29ea9
Rz Rasel <Rz Rasel>
2017-08-24 00:34:04 +0600

    Add Library Rz Library Dynamic Model Adapter

--- app/src/main/java/com/rz/sparkedandlibrary/ActSplash.java
+++ app/src/main/java/com/rz/sparkedandlibrary/ActSplash.java
@@ -29,36 +29,3 @@ public class ActSplash extends AppCompatActivity {
         sysTvDeviceInfo.setText(stringBuilder.toString());
     }
 }
-/*
-
-
-Map<String, String> map = ...
-for (Map.Entry<String, String> entry : map.entrySet())
-{
-    System.out.println(entry.getKey() + "/" + entry.getValue());
-}
-
-
-
-Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-Iterator<Map.Entry<Integer, Integer>> entries = map.entrySet().iterator();
-while (entries.hasNext()) {
-    Map.Entry<Integer, Integer> entry = entries.next();
-    System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
-}
-
-
-Iterator it = mp.entrySet().iterator();
-while (it.hasNext()) {
-    Map.Entry pair = (Map.Entry)it.next();
-    System.out.println(pair.getKey() + " = " + pair.getValue());
-    it.remove(); // avoids a ConcurrentModificationException
-}
-
-
-Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-for (Integer key : map.keySet()) {
-    Integer value = map.get(key);
-    System.out.println("Key = " + key + ", Value = " + value);
-}
-*/
