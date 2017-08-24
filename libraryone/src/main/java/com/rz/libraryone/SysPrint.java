c5c299f0a79f04af0a972490427e8547a7efff8b
Developer <developer@Shurjo.local>
2017-08-24 15:42:19 +0600

    Add Library Project File

---
+++ libraryone/src/main/java/com/rz/libraryone/SysPrint.java
@@ -0,0 +1,11 @@
+package com.rz.libraryone;
+
+/**
+ * Created by developer on 8/24/17.
+ */
+
+public class SysPrint {
+    public SysPrint(String argStrMsg) {
+        System.out.println("|----|" + argStrMsg);
+    }
+}
